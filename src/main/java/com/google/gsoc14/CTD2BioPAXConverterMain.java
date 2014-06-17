d1566f9ca86b7b0b4ba8ef02349db92b1fa95636
B. Arman Aksoy <arman@aksoy.org>
2014-06-17 09:50:51 -0400

    basic maven project with paxtools dependency.

---
+++ src/main/java/com/google/gsoc14/CTD2BioPAXConverterMain.java
@@ -0,0 +1,13 @@
+package com.google.gsoc14;
+
+/**
+ * Hello world!
+ *
+ */
+public class CTD2BioPAXConverterMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/google/gsoc14/CTD2BioPAXConverterTest.java
@@ -0,0 +1,22 @@
+package com.google.gsoc14;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class CTD2BioPAXConverterTest extends TestCase {
+    public CTD2BioPAXConverterTest(String testName)
+    {
+        super( testName );
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite( CTD2BioPAXConverterTest.class );
+    }
+
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
