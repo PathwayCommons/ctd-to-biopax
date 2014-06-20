79cc400d144d2cfb503a83cbecc67a8cd1ef1eca
B. Arman Aksoy <arman@aksoy.org>
2014-06-20 10:05:16 -0400

    - Now creating top pathways that represents organism-specific information - Bindings/Co-treatments are now being handled just fine (Complexes and co-Controls) - Pubmed IDs are now being assigned - We have EXP reactions being created, but still need to implement others.

--- src/main/java/com/google/gsoc14/ctd2biopax/converter/CTDInteractionConverter.java
+++ src/main/java/com/google/gsoc14/ctd2biopax/converter/CTDInteractionConverter.java
@@ -2,22 +2,28 @@ package com.google.gsoc14.ctd2biopax.converter;
 
 import com.google.gsoc14.ctd2biopax.util.CTDUtil;
 import com.google.gsoc14.ctd2biopax.util.model.ActorTypeType;
+import com.google.gsoc14.ctd2biopax.util.model.AxnCode;
 import com.google.gsoc14.ctd2biopax.util.model.GeneForm;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.biopax.paxtools.controller.PropertyEditor;
+import org.biopax.paxtools.controller.SimpleEditorMap;
+import org.biopax.paxtools.controller.Traverser;
+import org.biopax.paxtools.controller.Visitor;
+import org.biopax.paxtools.model.BioPAXElement;
+import org.biopax.paxtools.model.BioPAXLevel;
 import org.biopax.paxtools.model.Model;
 import org.biopax.paxtools.model.level3.*;
 import org.biopax.paxtools.model.level3.Process;
-import org.ctdbase.model.ActorType;
-import org.ctdbase.model.AxnType;
-import org.ctdbase.model.IxnSetType;
-import org.ctdbase.model.IxnType;
+import org.ctdbase.model.*;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 public class CTDInteractionConverter extends Converter {
@@ -33,13 +39,8 @@ public class CTDInteractionConverter extends Converter {
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
             interactions = (IxnSetType) ((JAXBElement) unmarshaller.unmarshal(inputStream)).getValue();
 
-            int count = 0;
             for (IxnType ixn : interactions.getIxn()) {
                 convertInteraction(model, ixn);
-                count++;
-
-                // TODO: remove this
-                if(count > 20) break;
             }
 
             log.info("There are " + interactions.getIxn().size() + " in the model.");
@@ -54,12 +55,66 @@ public class CTDInteractionConverter extends Converter {
     private Control convertInteraction(Model model, IxnType ixn) {
         // We are going to use the first actor to create the controller
         List<ActorType> actors = ixn.getActor();
-        Control control = createControllerFromActor(model, ixn, actors.get(0));
+        Control control = createControlFromActor(model, ixn, actors.get(0));
         Process process = createProcessFromAction(model, ixn, actors.get(1));
         control.addControlled(process);
         assignControlVocabulary(control, ixn.getAxn().iterator().next());
 
+        // Categorize according to the organism via pathways
+        for (TaxonType taxonType : ixn.getTaxon()) {
+            assignReactionToPathway(model, control, taxonType);
+        }
+
+        // Now annotate with publications
+        int count = 0;
+        for (ReferenceType referenceType : ixn.getReference()) {
+            PublicationXref publicationXref = create(PublicationXref.class, "pub_" + ixn.getId() + "_" + (++count));
+            publicationXref.setDb("Pubmed");
+            publicationXref.setId(referenceType.getPmid().toString());
+            process.addXref(publicationXref);
+            model.add(publicationXref);
+        }
+
+        // And viola!
         return control;
+    }
+
+    private void assignReactionToPathway(Model model, Control control, TaxonType taxonType) {
+        String taxonName = taxonType.getValue();
+        String orgTaxId = taxonType.getId();
+        String taxonId = "taxon_pathway_" + orgTaxId;
+        Pathway pathway = (Pathway) model.getByID(taxonId);
+        if(pathway == null) {
+            pathway = create(Pathway.class, taxonId);
+            assignName(taxonName + " pathway", pathway);
+
+            // Let's asign the biosource
+            BioSource bioSource = create(BioSource.class, "src_" + orgTaxId);
+            assignName(taxonName, bioSource);
+            pathway.setOrganism(bioSource);
+
+            UnificationXref unificationXref = create(UnificationXref.class, "xref_taxon_" + orgTaxId);
+            unificationXref.setDb("taxonomy");
+            unificationXref.setId(orgTaxId);
+            bioSource.addXref(unificationXref);
+
+            model.add(unificationXref);
+            model.add(bioSource);
+            model.add(pathway);
+        }
+        pathway.addPathwayComponent(control);
+
+        // Propagate pathway assignments
+        final Pathway finalPathway = pathway;
+        Traverser traverser = new Traverser(SimpleEditorMap.get(BioPAXLevel.L3), new Visitor() {
+            @Override
+            public void visit(BioPAXElement domain, Object range, Model model, PropertyEditor<?, ?> editor) {
+                if(range != null && range instanceof Process) {
+                    finalPathway.addPathwayComponent((Process) range);
+                }
+            }
+        });
+        traverser.traverse(control, model);
     }
 
     private void assignControlVocabulary(Control control, AxnType action) {
@@ -70,42 +125,99 @@ public class CTDInteractionConverter extends Converter {
             case '-':
                 control.setControlType(ControlType.INHIBITION);
                 break;
+            case '1':
             default:
                 // No vocab -- unknown
         }
     }
 
     private Process createProcessFromAction(Model model, IxnType ixn, ActorType actor) {
-        List<AxnType> actions = ixn.getAxn();
-        assert actions.size() == 1;
-        AxnType action = actions.iterator().next();
+        AxnCode axnCode = CTDUtil.extractAxnCode(ixn);
+        String processId = "process_" + ixn.getId();
+        Process process;
+        switch (axnCode) {
+            case EXP:
+                    process = createExpressionReaction(model, ixn, actor, processId);
+                break;
+            default:
+                Pathway pathway = create(Pathway.class, processId);
+                transferNames(ixn, pathway);
+                model.add(pathway);
+                process = pathway;
+        }
 
-        Pathway pathway = create(Pathway.class, "process_" + ixn.getId());
-        transferNames(ixn, pathway);
-        model.add(pathway);
-        return pathway;
+        return process;
     }
 
+    private Process createExpressionReaction(Model model, IxnType ixn, ActorType actor, String processId) {
+        TemplateReaction templateReaction = create(TemplateReaction.class, processId);
+        templateReaction.setTemplateDirection(TemplateDirectionType.FORWARD); // Expression is always forward
+        ActorTypeType actorTypeType = CTDUtil.extractActorTypeType(actor);
+        switch (actorTypeType) {
+            case IXN:
+                log.error("Found an expression reaction with the second " +
+                        "actor also a reaction, which is ambigous: #"
+                        + ixn.getId() + ". Created an incomplete template reaction.");
+                break;
+            default:
+                SimplePhysicalEntity actorEntity = createSPEFromActor(model, actor);
+                templateReaction.addProduct(actorEntity);
+                model.add(templateReaction);
+                transferNames(ixn, templateReaction);
+        }
+        return templateReaction;
+    }
 
-    private Control createControllerFromActor(Model model, IxnType ixn, ActorType actor) {
+    private Control createControlFromActor(Model model, IxnType ixn, ActorType actor) {
         Control control = create(Control.class, "control_" + ixn.getId());
-        Controller controller = createControllerFromActor(model, actor);
-        control.addController(controller);
+        for (Controller controller : createControlEntityFromActor(model, actor)) {
+            control.addController(controller);
+        }
         model.add(control);
+
         return control;
     }
 
-    // TODO: Some interactions, co-treatment, might create multiple controllers. Fix this.
-    private Controller createControllerFromActor(Model model, ActorType actor) {
-        ActorTypeType aType = ActorTypeType.valueOf(actor.getType().toUpperCase());
-        Controller controller;
+    private Collection<Controller> createControlEntityFromActor(Model model, ActorType actor) {
+        HashSet<Controller> controllers = new HashSet<Controller>();
+        ActorTypeType aType = CTDUtil.extractActorTypeType(actor);
+        switch (aType) {
+            case IXN:
+                IxnType ixnType = CTDUtil.convertActorToIxn(actor);
+                switch(CTDUtil.extractAxnCode(ixnType)) {
+                    case B:
+                        controllers.add(createComplex(model, ixnType));
+                        break;
+                    case W:
+                        controllers.addAll(createMultipleControllers(model, ixnType));
+                        break;
+                    default:
+                        Control control = convertInteraction(model, ixnType);
+                        Pathway pathway = createPathway(model, actor);
+                        pathway.addPathwayComponent(control);
+                        controllers.add(pathway);
+                }
+                break;
+            default: // If not an IXN, then it is a physical entity
+                controllers.add(createSPEFromActor(model, actor));
+                break;
+        }
+
+        return controllers;
+    }
+
+    private SimplePhysicalEntity createSPEFromActor(Model model, ActorType actor) {
+        SimplePhysicalEntity spe;
+        ActorTypeType aType = CTDUtil.extractActorTypeType(actor);
+
         switch (aType) {
             case GENE:
             default:
                 String form = actor.getForm();
-                GeneForm geneForm = form == null
-                        ? GeneForm.PROTEIN
-                        : GeneForm.valueOf(form.toUpperCase().replaceAll(" ", "_"));
+                GeneForm geneForm =
+                        form == null
+                                ? GeneForm.PROTEIN
+                                : GeneForm.valueOf(form.toUpperCase().replaceAll(" ", "_"));
 
                 Class<? extends SimplePhysicalEntity> eClass;
                 Class<? extends EntityReference> refClass;
@@ -116,6 +228,7 @@ public class CTDInteractionConverter extends Converter {
                         refClass = ProteinReference.class;
                         break;
                     case GENE:
+                    case MRNA:
                         eClass = Rna.class;
                         refClass = RnaReference.class;
                         break;
@@ -137,25 +250,47 @@ public class CTDInteractionConverter extends Converter {
                         break;
                 }
 
-                controller = createEntityFromActor(model, actor, eClass, refClass);
+                spe = createEntityFromActor(model, actor, eClass, refClass);
                 break;
             case CHEMICAL:
-                controller = createEntityFromActor(model, actor, SmallMolecule.class, SmallMoleculeReference.class);
-                break;
-            case IXN:
-                controller = createPathway(model, actor);
+                spe = createEntityFromActor(model, actor, SmallMolecule.class, SmallMoleculeReference.class);
                 break;
         }
 
-        return controller;
+        return spe;
     }
 
-    private Controller createPathway(Model model, ActorType actor) {
+    private Collection<? extends Controller> createMultipleControllers(Model model, IxnType ixnType) {
+        HashSet<Controller> controllers = new HashSet<Controller>();
+        for (ActorType actorType : ixnType.getActor()) {
+            controllers.addAll(createControlEntityFromActor(model, actorType));
+        }
+
+        return controllers;
+    }
+
+    private Complex createComplex(Model model, IxnType ixnType) {
+        Complex complex = create(Complex.class, "complex_" + ixnType.getId());
+        transferNames(ixnType, complex);
+
+        String cName = "";
+        for (ActorType actorType : ixnType.getActor()) {
+            SimplePhysicalEntity speFromActor = createSPEFromActor(model, actorType);
+            cName += speFromActor.getDisplayName() + "/";
+            complex.addComponent(speFromActor);
+        }
+        cName = cName.substring(0, cName.length()-1);
+        assignName(cName, complex);
+
+        model.add(complex);
+        return complex;
+    }
+
+
+    private Pathway createPathway(Model model, ActorType actor) {
         Pathway pathway = create(Pathway.class, actor.getId());
         transferNames(actor, pathway);
         model.add(pathway);
-        // TODO: if this is the case, then actor represents a rxn
-        // TODO: Create this reaction and add that to the pathway
         return pathway;
     }
 
@@ -167,7 +302,7 @@ public class CTDInteractionConverter extends Converter {
     ) {
         String actorTypeId = actorType.getId();
         String entityId = actorTypeId + "_" + actorType.getParentid();
-        String refId = "ref_" + actorTypeId;
+        String refId = "ref_" + actorType.getForm() + "_" + actorTypeId;
 
         EntityReference entityReference = (EntityReference) model.getByID(refId);
         if(entityReference == null) {
@@ -193,16 +328,11 @@ public class CTDInteractionConverter extends Converter {
     }
 
     private void transferNames(ActorType actor, Named named) {
-        String name = CTDUtil.extractName(actor);
-        assignName(name, named);
+        assignName(CTDUtil.extractName(actor), named);
     }
 
     private void transferNames(IxnType ixn, Named named) {
-        String fName = CTDUtil.extractName(ixn.getActor().get(0));
-        String sName = CTDUtil.extractName(ixn.getActor().get(1));
-        String actionStr = ixn.getAxn().iterator().next().getValue();
-        String completeName = fName + " " + actionStr + " " + sName;
-        assignName(completeName, named);
+        assignName(CTDUtil.extractName(ixn), named);
     }
 
 
--- src/main/java/com/google/gsoc14/ctd2biopax/util/CTDUtil.java
+++ src/main/java/com/google/gsoc14/ctd2biopax/util/CTDUtil.java
@@ -1,12 +1,47 @@
 package com.google.gsoc14.ctd2biopax.util;
 
-import com.google.gsoc14.ctd2biopax.util.model.GeneForm;
+import com.google.gsoc14.ctd2biopax.util.model.ActorTypeType;
+import com.google.gsoc14.ctd2biopax.util.model.AxnCode;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.ctdbase.model.ActorType;
+import org.ctdbase.model.AxnType;
+import org.ctdbase.model.IxnType;
+import org.ctdbase.model.ObjectFactory;
 
+import javax.xml.bind.JAXBElement;
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.List;
 
 public class CTDUtil {
+    private static Log log = LogFactory.getLog(CTDUtil.class);
+    private static ObjectFactory ctdFactory = new ObjectFactory();
+
+    public static String extractName(IxnType ixn) {
+        String actionStr = ixn.getAxn().iterator().next().getValue();
+
+        String completeName = "";
+        switch (extractAxnCode(ixn)) {
+            case B:
+            case W:
+                Iterator<ActorType> iterator = ixn.getActor().iterator();
+                assert iterator.hasNext();
+                completeName = extractName(iterator.next()) + " " + actionStr + " ";
+                while(iterator.hasNext()) {
+                    completeName += extractName(iterator.next()) + ", ";
+                }
+                completeName = completeName.substring(0, completeName.length()-2);
+                break;
+            default:
+                String fName = CTDUtil.extractName(ixn.getActor().get(0));
+                String sName = CTDUtil.extractName(ixn.getActor().get(1));
+                completeName = fName + " " + actionStr + " " + sName;
+        }
+
+        return completeName;
+    }
+
     public static String extractName(ActorType actor) {
         // TODO: Better name extraction
         List<Serializable> serializableList = actor.getContent();
@@ -21,7 +56,45 @@ public class CTDUtil {
 
         return serializableList.size() == 1
                 ? serializableList.iterator().next().toString() + formStr
-                : "complex interaction";
+                : "[" + extractName(convertActorToIxn(actor)) + "]";
 
+    }
+    public static AxnType extractAxn(IxnType ixn) {
+        List<AxnType> actions = ixn.getAxn();
+        assert actions.size() > 0;
+        return actions.iterator().next();
+    }
+
+    public static AxnCode extractAxnCode(AxnType axnType) {
+        return AxnCode.valueOf(axnType.getCode().toUpperCase());
+    }
+
+    public static AxnCode extractAxnCode(IxnType ixn) {
+        return extractAxnCode(extractAxn(ixn));
+    }
+
+    public static IxnType convertActorToIxn(ActorType actor) {
+        IxnType ixnType = ctdFactory.createIxnType();
+
+        for (Serializable item : actor.getContent()) {
+            if(!(item instanceof JAXBElement)) { continue; }
+
+            Object value = ((JAXBElement) item).getValue();
+            if(value instanceof ActorType) {
+                ixnType.getActor().add((ActorType) value);
+            } else if(value instanceof AxnType) {
+                ixnType.getAxn().add((AxnType) value);
+            } else {
+                log.error("Nested item with unexpected class: " + value.getClass().getSimpleName());
+            }
+        }
+
+        ixnType.setId(actor.getId().hashCode());
+        return ixnType;
+    }
+
+
+    public static ActorTypeType extractActorTypeType(ActorType actor) {
+        return ActorTypeType.valueOf(actor.getType().toUpperCase());
     }
 }
